5c0dd8f7be4b6618f2647a290b26d7ae6a296cfe
Michael <michaelzhaihk@gmail.com>
2016-08-19 14:53:26 +1200

    init project

---
+++ XTYModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTYModel
+//
+//  Created by Michael on 16/8/19.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XTYModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XTYModel
+//
+//  Created by Michael on 16/8/19.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
